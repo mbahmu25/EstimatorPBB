497c1f1949773c9a5f7007cb8972ba9598069e93
mbahmu25 <nikogenta25@gmail.com>
2025-06-04 12:41:11 +0700

    hampir final

--- estimator.py
+++ estimator.py
@@ -1,4 +1,5 @@
 from qgis.PyQt.QtCore import QCoreApplication
+
 from qgis.core import (
     QgsProcessing,
     QgsProcessingAlgorithm,
@@ -10,19 +11,33 @@ from qgis.core import (
     QgsProcessingOutputLayerDefinition,
     QgsVectorLayer,
     QgsProject,
-    QgsProcessingParameterField
+    QgsProcessingParameterField,
+    QgsProcessingParameterNumber
 )
+from qgis.utils import iface
 import geopandas as gpd
 import tempfile
 import os
+import json
+import random
+
+# from .report import get_report
+
 
 
 class GeoPandasSpatialJoin(QgsProcessingAlgorithm):
     PBT = 'Bidang Tanah'
     BG = "Bangunan"
     ZNT = 'Zona Nilai Tanah'
+    ADM = 'Batas Administrasi dan PBB'
+    DAERAH = 'Daerah'
+    PBBExist = 'PBB Eksisting'
+    NJOP_BG = 'NJOP_BG'
     PREDICATE = 'PREDICATE'
     OUTPUT = 'OUTPUT'
+    REPORT = 'REPORT'
+    TARIF = "Tarif"
+    HAK = "HAK"
 
     def tr(self, string):
         return QCoreApplication.translate('Processing', string)
@@ -47,9 +62,41 @@ class GeoPandasSpatialJoin(QgsProcessingAlgorithm):
 
     def initAlgorithm(self, config=None):
         self.addParameter(QgsProcessingParameterFeatureSource(
+            self.ADM, self.tr('Batas Administrasi dan PBB')))
+        self.addParameter(
+            QgsProcessingParameterField(
+                'DAERAH',  # Gunakan nama unik untuk parameter field
+                self.tr('Kolom Nama Daerah'),
+                parentLayerParameterName=self.ADM  # Ini referensi ke input layer
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterField(
+                'PBBExist',  # Gunakan nama unik untuk parameter field
+                self.tr('PBB Eksisting'),
+                parentLayerParameterName=self.ADM  # Ini referensi ke input layer
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterField(
+                'TARIF_DAERAH',  # Gunakan nama unik untuk parameter field
+                self.tr('Tarif'),
+                parentLayerParameterName=self.ADM  # Ini referensi ke input layer
+            )
+        )
+        self.addParameter(QgsProcessingParameterFeatureSource(
             self.PBT, self.tr('Bidang Tanah')))
         self.addParameter(QgsProcessingParameterFeatureSource(
             self.BG, self.tr('Bangunan')))
+        self.addParameter(QgsProcessingParameterNumber('NJOP_BG',self.tr('Nilai Pajak Bangunan'), type=QgsProcessingParameterNumber.Double, defaultValue=0.1))
+        self.addParameter(
+            QgsProcessingParameterField(
+                'BUILDING_LEVEL_FIELD',  # Gunakan nama unik untuk parameter field
+                self.tr('Building Level [optional]'),
+                parentLayerParameterName=self.BG,  # Ini referensi ke input layer
+                optional = True
+            )
+        )
         self.addParameter(QgsProcessingParameterFeatureSource(
             self.ZNT, self.tr('Zona Nilai Tanah')))
         
@@ -60,25 +107,37 @@ class GeoPandasSpatialJoin(QgsProcessingAlgorithm):
                 parentLayerParameterName=self.ZNT  # Ini referensi ke input layer
             )
         )
+        
         self.addParameter(QgsProcessingParameterFileDestination(
             self.OUTPUT, self.tr('Output file'), fileFilter='ESRI Shapefile (*.shp)'))
-
+        self.addParameter(QgsProcessingParameterFileDestination(
+            self.REPORT, self.tr('Report Output file'), fileFilter='HTML (*.html)'))
     def processAlgorithm(self, parameters, context, feedback):
+        admin_layer = self.parameterAsVectorLayer(parameters, self.ADM, context)
         input_layer = self.parameterAsVectorLayer(parameters, self.PBT, context)
         join_layer = self.parameterAsVectorLayer(parameters, self.ZNT, context)
         bg_layer = self.parameterAsVectorLayer(parameters,self.BG,context)
+        field_adm = self.parameterAsString(parameters, 'DAERAH', context)
+        field_PBBExist = self.parameterAsString(parameters, 'PBBExist', context)
+        field_tarif = self.parameterAsString(parameters, 'TARIF_DAERAH', context)
         field_name = self.parameterAsString(parameters, 'FIELD_ZNT', context)
+        field_building_level = self.parameterAsDouble(parameters,'BUILDING_LEVEL',context)
+        nilai_bg = self.parameterAsDouble(parameters, 'NJOP_BG', context)
         output_path = self.parameterAsFileOutput(parameters, self.OUTPUT, context)
+        report_output_path = self.parameterAsFileOutput(parameters, self.REPORT, context)
 
         if input_layer is None or join_layer is None:
             raise QgsProcessingException("Invalid input or join layer.")
 
         # Export both layers to temp files
         tmp_dir = tempfile.mkdtemp()
+        tmpAdm = os.path.join(tmp_dir, 'admn.gpkg')
         tmp1 = os.path.join(tmp_dir, 'input1.gpkg')
         tmp2 = os.path.join(tmp_dir, 'input2.gpkg')
         tmp3 = os.path.join(tmp_dir, 'input3.gpkg')
-
+        _ = QgsVectorFileWriter.writeAsVectorFormat(
+            admin_layer, tmpAdm, "utf-8", input_layer.crs(), "GPKG"
+        )
         _ = QgsVectorFileWriter.writeAsVectorFormat(
             input_layer, tmp1, "utf-8", input_layer.crs(), "GPKG"
         )
@@ -93,11 +152,23 @@ class GeoPandasSpatialJoin(QgsProcessingAlgorithm):
         pbt = gpd.read_file(tmp1)
         znt = gpd.read_file(tmp2)
         bg = gpd.read_file(tmp3)
+        admin = gpd.read_file(tmpAdm)
         znt = znt.to_crs(pbt.crs)
         bg = bg.to_crs(pbt.crs)
+        admin = admin.to_crs(pbt.crs)
 
         pbt = pbt.reset_index(drop=True)
         pbt['id_bidang_internal'] = pbt.index
+
+        # Merge Batas Admin dengan PBT
+        pbt_adm = gpd.overlay(pbt, admin[['geometry', field_adm, field_tarif, field_PBBExist]], how='intersection')
+
+        # Ambil admin dengan luas terbesar (dominasi)
+        pbt_adm['luas'] = pbt_adm.geometry.area
+        pbt_adm = pbt_adm.sort_values('luas', ascending=False).drop_duplicates('id_bidang_internal')
+
+        # Merge hasil ke bidang tanah
+        pbt = pbt.merge(pbt_adm[['id_bidang_internal', field_adm, field_tarif, field_PBBExist]], on='id_bidang_internal', how='left')
 
         # Perhitungan ZNT per Bidang Tanah
         pbt_znt = gpd.overlay(pbt, znt[['geometry', field_name]], how='intersection')
@@ -120,9 +191,13 @@ class GeoPandasSpatialJoin(QgsProcessingAlgorithm):
         pbt_final = pbt_final.drop(columns=[col for col in pbt_final.columns if col.startswith('index_')], errors='ignore')
 
         # Perhitungan PBB
-        pbt_final['pajak_bangunan'] = pbt_final['luas_bangunan']*10000 # Asumsi setara masih bisa diganti berdasarkan data
-        pbt_final['pajak_bumi'] = pbt_final['luas_persil']*20000 # Asumsi setara masih bisa diganti berdasarkan data
-        pbt_final['PBB'] = pbt_final['pajak_bangunan']+pbt_final['pajak_bumi']
+        if field_building_level:
+            pbt_final['pajak_bangunan'] = pbt_final[field_building_level].fillna(1)*pbt_final['luas_bangunan']*nilai_bg 
+        else:
+            pbt_final['pajak_bangunan'] = pbt_final['luas_bangunan']*nilai_bg
+
+        pbt_final['pajak_bumi'] = pbt_final['luas_persil']*pbt_final[field_name] # Asumsi setara masih bisa diganti berdasarkan data
+        pbt_final['PBB'] = pbt_final[field_tarif]*(pbt_final['pajak_bangunan']+pbt_final['pajak_bumi'])
         # Save to output
         pbt_final.to_file(output_path, driver='ESRI Shapefile')
 
@@ -131,8 +206,171 @@ class GeoPandasSpatialJoin(QgsProcessingAlgorithm):
         if not layer.isValid():
             raise QgsProcessingException("Output layer is not valid.")
         QgsProject.instance().addMapLayer(layer)
+        outputData = dict()
+        outputData["luas_area"] = sum(pbt_final.geometry.area)
+        outputData["total_bangunan"] = len(bg)
+        outputData["total_bidang"] = len(pbt)
+        outputData["pajak_bangunan"] = sum(pbt_final['pajak_bangunan'])
+        outputData["pajak_bumi"] = sum(pbt_final['pajak_bumi'])
+        outputData["total_pbb_eksisting"] = sum(pbt_final[field_PBBExist])
+        outputData["total_pbb"] = sum(pbt_final['pajak_bangunan'])+sum(pbt_final['pajak_bumi'])
+        outputData["tabel"]=[]
         
+        pbb_per_daerah = pbt_final.groupby(field_adm)[[field_PBBExist,"PBB"]].sum().reset_index().rename(columns={field_adm: 'daerah', field_PBBExist: 'pbb_eksisting', "PBB":"pbb_estimasi"}).to_dict(orient="records")
+
+        labels = [d['daerah'] for d in pbb_per_daerah]
+        values = [d['pbb_eksisting'] for d in pbb_per_daerah]
+        values2 = [d['pbb_estimasi'] for d in pbb_per_daerah]
+        colors = [f"#{random.randint(0, 0xFFFFFF):06x}" for _ in pbb_per_daerah]
+        colors2 = [f"#{random.randint(0, 0xFFFFFF):06x}" for _ in pbb_per_daerah]
+
+        # Buat nested dataset: misalnya 1 level (parent), 1 level (child)
+        chart_data = {
+            "labels": labels,
+            "datasets": [
+                {
+                    "label": "PBB Eksisting Daerah",
+                    "data": values,
+                    "backgroundColor": colors
+                },
+                {
+                    "label": "PBB Estimasi Daerah",
+                    "data": values2,
+                    "backgroundColor": colors2
+                }
+            ]
+        }
+
         
+
+        # Output JSON string untuk disisipkan ke HTML
+        chart_data_js = json.dumps(chart_data)
+
+
+        table ="" 
+        for i in pbb_per_daerah:
+            table+=f'<tr><td>{i["daerah"]}</td><td>Rp {i["pbb_eksisting"]:,}</td><td>Rp {i["pbb_estimasi"]:,}</td><td>{i["pbb_estimasi"]/i["pbb_eksisting"]*100}</td></tr>'
+
+        html_content = """
+        <html>
+            <head>
+                <script src="https://cdn.tailwindcss.com"></script>
+                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+            </head>
+        """+f"""
+            <body class="font-sans p-8 text-gray-600 bg-gray-50">
+            <div
+                class="bg-white mx-auto my-8 px-10 py-12 shadow-md rounded-md print:shadow-none print:rounded-none print:my-0 print:p-0"
+                style="width: 794px; min-height: 1123px"
+            >
+                <h2 class="text-2xl font-semibold text-blue-600 mb-1">Informasi Data</h2>
+                <h3 class="text-lg font-semibold text-black mb-1">Bidang Tanah</h3>
+
+                <table class="table-auto border-collapse border-0 mb-6">
+                    <tbody>
+                        <tr>
+                        <td class="pr-3 text-gray-700 text-[16pt] font-normal">Luas Area</td>
+                        <td class="px-2">:</td>
+                        <td class="text-[16pt] font-normal text-gray-900">
+                            {outputData['luas_area']:.2f} m<sup>2</sup>
+                        </td>
+                        </tr>
+                        <tr>
+                        <td class="pr-3 text-gray-700 text-[16pt] font-normal">Total Bangunan</td>
+                        <td class="px-2">:</td>
+                        <td class="text-[16pt] font-normal text-gray-900">{outputData['total_bangunan']}</td>
+                        </tr>
+                        <tr>
+                        <td class="pr-3 text-gray-700 text-[16pt] font-normal">
+                            Total Bidang Tanah
+                        </td>
+                        <td class="px-2">:</td>
+                        <td class="text-[16pt] font-normal text-gray-900">{outputData['total_bidang']}</td>
+                        </tr>
+                        <tr>
+                        <td class="pr-3 text-gray-700 text-[16pt] font-normal">
+                            Total Pajak Bangunan
+                        </td>
+                        <td class="px-2">:</td>
+                        <td class="text-[16pt] font-normal text-gray-900">Rp {int(outputData['pajak_bangunan']):,}</td>
+                        </tr>
+                        <tr>
+                        <td class="pr-3 text-gray-700 text-[16pt] font-normal">
+                            Total Pajak Bumi
+                        </td>
+                        <td class="px-2">:</td>
+                        <td class="text-[16pt] font-normal text-gray-900">Rp {int(outputData['pajak_bumi']):,}</td>
+                        </tr>
+                        <tr>
+                        <td class="pr-3 text-gray-700 text-[16pt] font-normal">Total PBB</td>
+                        <td class="px-2">:</td>
+                        <td class="text-[16pt] font-normal text-gray-900">Rp {int(outputData['total_pbb']):,}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            """+f"""
+            <div class="max-h-48 overflow-y-auto border border-gray-300 bg-white p-4 rounded shadow-sm">
+                <h3 class="text-lg font-semibold text-black mb-2">Tabel Pajak</h3>
+                <table class="w-full border-collapse">
+                    <thead>
+                    <tr class="bg-gray-200 text-gray-700 text-center text-sm">
+                        <th class="border border-gray-300 px-3 py-1">Nama Daerah</th>
+                        <th class="border border-gray-300 px-3 py-1">PBB Eksisting</th>
+                        <th class="border border-gray-300 px-3 py-1">PBB Estimasi</th>
+                        <th class="border border-gray-300 px-3 py-1">Perubahan (%)</th>
+                    </tr>
+                    </thead>
+                    <tbody id="table-body" class="text-gray-600 text-sm">
+                    {table}
+                    </tbody>
+                </table>
+            </div>
+                <div class="mt-8 max-w-xs mx-auto">
+                    <h3 class="text-lg font-semibold text-black mb-3 text-center">
+                        Pie Chart Pajak
+                    </h3>
+                    <canvas id="nestedPieChart" class="mx-auto"></canvas>
+                </div>
+            </div>
+        </body>
+        </html>
+        """+"""
+    <script>
+
+      const ctx = document.getElementById("nestedPieChart").getContext("2d");
+      """+f"""
+      const chartData = {chart_data}
+      """+"""
+
+      new Chart(ctx, {
+        type: 'doughnut',
+        data: chartData,
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: "bottom",
+              labels: { font: { size: 14 }, color: "#000" },
+            },
+            tooltip: {
+              callbacks: {
+                label: function (context) {
+                  return (
+                    context.label +
+                    ": Rp " +
+                    context.parsed.toLocaleString("id-ID")
+                  );
+                },
+              },
+            },
+          },
+        },
+      });
+    </script>
+"""
+        with open(report_output_path,"w") as r:
+           r.write(html_content)
+
         return {self.OUTPUT: output_path}
 
 
--- report.py
+++ report.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import (
     QDialog, QVBoxLayout, QLabel, QPushButton, QTextBrowser,
-    QWidget, QScrollArea
+    QWidget, QScrollArea,
+    QDockWidget
 )
 from qgis.utils import iface
 from PyQt5.QtGui import QPixmap
@@ -11,12 +12,54 @@ from io import BytesIO
 
 
 # === Dialog class ===
-class HtmlReportDialog(QDialog):
-    def __init__(self, parent=None):
+class HtmlReportDialog(QDockWidget):
+    def group(self):
+        return self.tr('Pelatihan BPN')
+    def groupId(self):
+        return 'report'
+    def __init__(self, data = {
+            'luas_area': 12345,
+            'total_bangunan': 15,
+            'total_bidang': 30,
+            'pajak_bangunan': 12345678,
+            'pajak_bumi': 9876543,
+            'total_pbb': 22222222,
+            'mean_bumi':0,
+            'mean_bangunan':0,
+            "mean_pbb":0,
+            'tabel':[
+                {
+                "nama_daerah":"Cokro",
+                "pajak_bangunan":0,
+                "pajak_bumi":0,
+                "pbb":0
+                },{
+                "nama_daerah":"Cokro",
+                "pajak_bangunan":0,
+                "pajak_bumi":0,
+                "pbb":0
+                },{
+                "nama_daerah":"Cokro",
+                "pajak_bangunan":0,
+                "pajak_bumi":0,
+                "pbb":0
+                },
+                {
+                "nama_daerah":"Cokro",
+                "pajak_bangunan":0,
+                "pajak_bumi":0,
+                "pbb":0
+                },
+                {
+                "nama_daerah":"Cokro",
+                "pajak_bangunan":0,
+                "pajak_bumi":0,
+                "pbb":0
+                }
+            ]
+        },parent=None):
+        # data 
         
-        isian ="<h1>{a}</h1>"
-        a = 100*123125
-        isian += f"<h1>{a}</h1>"
         html_content = """
         <html>
         <head>
@@ -57,6 +100,7 @@ class HtmlReportDialog(QDialog):
                 font-size: 8pt;
             }
         </style>
+        """+f"""
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         </head>
         <body>
@@ -66,130 +110,131 @@ class HtmlReportDialog(QDialog):
               <tr>
                 <td class="text">Luas Area</td>
                 <td class="text">:</td>
-                <td class="text">12345 m<sup>2</sup></td>
+                <td class="text">{data['luas_area']} m<sup>2</sup></td>
               </tr>
               <tr>
                 <td class="text">Total Bangunan</td>
                 <td class="text">:</td>
-                <td class="text">15</td>
+                <td class="text">{data['total_bangunan']}</td>
               </tr>
               <tr>
                 <td class="text">Total Bidang Tanah</td>
                 <td class="text">:</td>
-                <td class="text">30</td>
+                <td class="text">{data['total_bidang']}</td>
               </tr>
               <tr>
                 <td class="text">Total Pajak Bangunan</td>
                 <td class="text">:</td>
-                <td class="text">Rp 12.345.678</td>
+                <td class="text">Rp {int(data['pajak_bangunan']):,}</td>
               </tr>
               <tr>
                 <td class="text">Total Pajak Bumi</td>
                 <td class="text">:</td>
-                <td class="text">Rp 9.876.543</td>
+                <td class="text">Rp {int(data['pajak_bumi']):,}</td>
               </tr>
               <tr>
                 <td class="text">Total PBB</td>
                 <td class="text">:</td>
-                <td class="text">Rp 22.222.222</td>
+                <td class="text">Rp {int(data['total_pbb']):,}</td>
               </tr>
               <tr>
                 <td class="text">Rata-rata Pajak Bangunan</td>
                 <td class="text">:</td>
-                <td class="text">Rp 1.234.567</td>
+                <td class="text">Rp {int(data['mean_bangunan'])}</td>
               </tr>
               <tr>
                 <td class="text">Rata-rata Pajak Bumi</td>
                 <td class="text">:</td>
-                <td class="text">Rp 987.654</td>
+                <td class="text">Rp {int(data['mean_bumi']):,}</td>
               </tr>
               <tr>
                 <td class="text">Rata-rata PBB</td>
                 <td class="text">:</td>
-                <td class="text">Rp 1.111.111</td>
+                <td class="text">Rp {int(data['mean_pbb']):,}</td>
               </tr>
             </table>
 
             <div class="scroll-table">
                 <h3>Tabel Pajak</h3>
+            """+"""
                 <style>
-                th, td {
-                border: 1px solid #ccc; padding: 6px; text-align: center; color: #333;
-                background-color: white;
-            }
-            th {
-                background-color: #f0f0f0;
-            }
-            td.left {
-                text-align: left;
-                font-weight: normal;
-                color: #ACABAB;
-            }
-            td.center {
-                text-align: center;
-                color: #ACABAB;
-            }
-            td.right {
-                text-align: right;
-                color: #ACABAB;
-            }
-            </style>
+                    th, td {
+                    border: 1px solid #ccc; padding: 6px; text-align: center; color: #333;
+                    background-color: white;
+                }
+                th {
+                    background-color: #f0f0f0;
+                }
+                td.left {
+                    text-align: left;
+                    font-weight: normal;
+                    color: #ACABAB;
+                }
+                td.center {
+                    text-align: center;
+                    color: #ACABAB;
+                }
+                td.right {
+                    text-align: right;
+                    color: #ACABAB;
+                }
+                </style>
+            """+f"""
                 <table>
                     <thead>
                         <tr>
-                            <th>Desa</th>
+                            <th>Nama Daerah</th>
                             <th>Pajak Bangunan</th>
                             <th>Pajak Bumi</th>
                             <th>PBB</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr><td>Kelurahan 1</td><td>Rp 100.000</td><td>Rp 200.000</td><td>Rp 300.000</td></tr>
-                        <tr><td>Kelurahan 2</td><td>Rp 110.000</td><td>Rp 210.000</td><td>Rp 310.000</td></tr>
-                        <tr><td>Kelurahan 3</td><td>Rp 120.000</td><td>Rp 220.000</td><td>Rp 320.000</td></tr>
-                        <tr><td>Kelurahan 4</td><td>Rp 130.000</td><td>Rp 230.000</td><td>Rp 330.000</td></tr>
+                """
+        for i in data["tabel"]:
+            html_content+=f'<tr><td>{i["nama_daerah"]}</td><td>Rp {i["pajak_bangunan"]:,}</td><td>Rp {i["pajak_bumi"]:,}</td><td>Rp {i["pbb"]:,}</td></tr>'
+        html_content += f"""
                     </tbody>
                 </table>
             </div>
         </body>
         </html>
         """
-        super().__init__(parent)
-        self.setWindowTitle("Report PBB Daerah")
+        super().__init__("Report PBB Daerah", parent)
+        self.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
         self.setMinimumSize(600, 500)
 
-        # === Scroll Area utama ===
+        # === Widget utama dalam dock ===
+        main_widget = QWidget()
+        main_layout = QVBoxLayout(main_widget)
+
+        # === Scroll Area ===
         scroll_area = QScrollArea()
         scroll_area.setWidgetResizable(True)
 
-        # === Widget kontainer yang bisa discroll ===
         scroll_content = QWidget()
         scroll_layout = QVBoxLayout(scroll_content)
 
-        
-        
         self.browser = QTextBrowser()
         self.browser.setHtml(html_content)
         self.browser.setMinimumHeight(500)
 
         scroll_layout.addWidget(self.browser)
 
-        # === Masukkan scroll_content ke scroll_area ===
-        scroll_area.setWidget(scroll_content)
-
-        # === Layout utama dialog ===
-        main_layout = QVBoxLayout(self)
-        main_layout.addWidget(scroll_area)
-
-        # Tombol Close di bawah (tetap terlihat)
-        self.close_button = QPushButton("Close")
-        self.close_button.clicked.connect(self.accept)
-        main_layout.addWidget(self.close_button)
-        
-        # === Chart ===
+        # Chart
         self.chart_label = QLabel()
         self.chart_label.setAlignment(Qt.AlignCenter)
         scroll_layout.addWidget(self.chart_label)
+
+        scroll_area.setWidget(scroll_content)
+        main_layout.addWidget(scroll_area)
+
+        # Tombol Close (dock bisa ditutup manual juga dari UI)
+        self.close_button = QPushButton("Close")
+        self.close_button.clicked.connect(self.close)
+        main_layout.addWidget(self.close_button)
+
+        self.setWidget(main_widget)
 
         # Render chart
         self.render_pie_chart()
@@ -215,5 +260,7 @@ class HtmlReportDialog(QDialog):
 
         plt.close(fig)
 # === Show dialog in QGIS ===
-dialog = HtmlReportDialog(parent=iface.mainWindow())
-dialog.exec_()
+# dialog = HtmlReportDialog(parent=iface.mainWindow())
+# iface.addDockWidget(Qt.RightDockWidgetArea, dialog)
+# dialog.show()
+
